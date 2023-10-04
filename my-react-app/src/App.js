ceaf72590a8a1748c39e8a48f6cfac062fa7079e
Zhuyun Wang <zwan6606@uni.sydney.edu.au>
2023-10-04 17:19:33 +1100

    react bootstrap 1104 1719

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,27 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import HomePage from './layouts/HomePage';
+import Navigator from './layouts/Navigator';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          alumni 
-          test here 10/04 11:32
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <HomePage />
+    <Navigator/>
+    </>
   );
 }
 
-export default App;
+export default App
