d7cb1ff4a1342a329e5732fd1045eb815db29caa
Zhuyun Wang <zwan6606@uni.sydney.edu.au>
2023-10-04 20:00:27 +1100

    bootstrap 1004 2000

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import HomePage from './layouts/HomePage';
 import Navigator from './layouts/Navigator';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <>
-    <HomePage />
     <Navigator/>
+    <HomePage />
+    
     </>
   );
 }
--- my-react-app/src/layouts/Navigator/index.jsx
+++ my-react-app/src/layouts/Navigator/index.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { Container, Navbar, Nav, Button } from 'react-bootstrap';
+
 
 export default function index() {
   return (
-    <div>
-      Navigator 
-    </div>
-  )
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Alumni Circle</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+
+          <Button>Register</Button>
+
+          <Button>Sign In</Button>
+
+        </Container>
+      </Navbar>
+      
+    </>
+  );
 }
