df325750ec59a7c7e9516dbcf3df04caaafc420a
su-kaihang <harryskh@foxmail.com>
2023-10-17 23:53:55 +1100

    手机验证码登录界面

--- my-react-app/src/layouts/Login/Login.jsx
+++ my-react-app/src/layouts/Login/Login.jsx
@@ -1,85 +1,129 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Nav, Navbar, Button, Card, Col, Form, Row } from 'react-bootstrap';
 
 export default function Login() {
   const [isPhoneLogin, setIsPhoneLogin] = useState(false);
+  const [showVerification, setShowVerification] = useState(false);
+  const [countdown, setCountdown] = useState(60);
+  const [timer, setTimer] = useState(null);
 
   const imageStyle = {
-    margin: '5px',
-    position: 'relative',
-    bottom: '220px',
-    left: '900px',
+    width: '100%',
+    height: 'auto',
   };
   const cardStyle = {
-    width: '800px',
-    height: '200px',
-    position: 'fixed',
-    top: '500px',
-    left: '90px',
-    margin: 'auto',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
+    width: '100%',
+    height: '100vh',
   };
 
   const handleToggleLogin = () => {
     setIsPhoneLogin(!isPhoneLogin);
+    setShowVerification(false); // Hide the verification input
   };
+
+  const handleGetVerificationCode = () => {
+    setShowVerification(true);
+    setCountdown(60); // Reset countdown
+
+    const timer = setInterval(() => {
+      if (countdown > 1) {
+        setCountdown((prevCountdown) => prevCountdown - 1); // Use closure to ensure using the latest countdown value
+      } else {
+        clearInterval(timer);
+        setShowVerification(false); // Hide the verification input
+      }
+    }, 1000);
+    setTimer(timer);
+  };
+
+  useEffect(() => {
+    // Clear the timer
+    return () => {
+      if (timer) {
+        clearInterval(timer);
+      }
+    };
+  }, [timer]);
 
   return (
     <div style={{ position: 'fixed', top: '0', right: '0', bottom: '0', left: '0', overflow: 'auto', backgroundColor: 'white' }}>
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
+          <Navbar.Brand href="/"><img src="/Logo.png" alt="" style={{width: '40%'}} /></Navbar.Brand>
           <Navbar.Brand href="/">Alumni Circle</Navbar.Brand>
-          <Navbar.Brand>Logo</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="/">About us</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
 
-      <div style={cardStyle}>
-        <Card bg="light" text="dark">
-          <Card.Body>
-            <Container>
-              <Row>
-                <Col md={6}>
-                  <h1>{isPhoneLogin ? 'Phone Login' : 'Login'}</h1>
-                  <Form>
+      <div style={{ display: 'flex', width: '100%' }}>
+        <div style={{ flex: 1, display: 'flex', justifyContent: 'center' }}>
+          <Card bg="light" text="dark" style={cardStyle}>
+            <Card.Body>
+              <Container>
+                <Row>
+                  <Col md={6}>
+                    <h1>{isPhoneLogin ? 'Phone Login' : 'Login'}</h1>
+                    <Form>
                     {isPhoneLogin ? (
                       <Form.Group controlId="phone">
                         <Form.Label>Phone Number</Form.Label>
-                        <Form.Control type="text" placeholder="Enter your phone number" />
+                        <div style={{ display: 'flex' }}>
+                          <Form.Control type="text" placeholder="Enter your phone number" />
+                        </div>
                       </Form.Group>
-                    ) : (
-                      <>
-                        <Form.Group controlId="username">
-                          <Form.Label>Username</Form.Label>
-                          <Form.Control type="text" placeholder="Enter your username" />
-                        </Form.Group>
-                        <Form.Group controlId="password">
-                          <Form.Label>Password</Form.Label>
-                          <Form.Control type="password" placeholder="Enter your password" />
-                        </Form.Group>
-                      </>
-                    )}
+                    ): (
+                        <>
+                          <Form.Group controlId="username">
+                            <Form.Label>Username</Form.Label>
+                            <Form.Control type="text" placeholder="Enter your username" />
+                          </Form.Group>
+                          <Form.Group controlId="password" >
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Enter your password" />
+                          </Form.Group>
+                          <Button variant="primary" type="submit" style={{marginTop: '5px'}}>
+                            Login
+                          </Button>
+                        </>
+                      )}
 
-                    <Button variant="primary" type="submit" style={{ marginTop: '10px' }}>
-                      {isPhoneLogin ? 'Send Verification Code' : 'Login'}
+                      {showVerification ? (
+                        <>
+                          <Form.Group controlId="verification">
+                            <Form.Label>Verification Code</Form.Label>
+                            <Form.Control type="text" placeholder="Enter verification code" />
+                            {showVerification ? (
+                            <div style={{ position: 'absolute', top: '0', right: '0', bottom: '0', display: 'flex', alignItems: 'center', paddingRight: '10px', color: 'lightgray' }}>
+                              {`(${countdown}s)`}
+                            </div>
+                          ) : null}
+                          </Form.Group>
+                          <Button variant="primary" type="submit" style={{marginTop: '5px'}}>
+                            Login
+                          </Button>
+                        </>
+                      ) : (
+                        isPhoneLogin && (
+                          <Button variant="primary" onClick={handleGetVerificationCode} style={{marginTop: '5px'}}>
+                            Get Verification Code ({countdown} seconds)
+                          </Button>
+                        )
+                      )}
+                    </Form>
+                    <Button variant="link" onClick={handleToggleLogin}>
+                      {isPhoneLogin ? 'Use Username and Password' : 'Use Phone Number'}
                     </Button>
-                  </Form>
-                  <Button variant="link" onClick={handleToggleLogin}>
-                    {isPhoneLogin ? 'Use Username and Password' : 'Use Phone Number'}
-                  </Button>
-                </Col>
-              </Row>
-              <div className="col-md-6">
-                <img src="/fb_share1.png" alt="" style={imageStyle} />
-              </div>
-            </Container>
-          </Card.Body>
-        </Card>
+                  </Col>
+                </Row>
+              </Container>
+            </Card.Body>
+          </Card>
+        </div>
+        <div style={{ flex: 1, display: 'flex', justifyContent: 'center' }}>
+          <img src="/fb_share1.png" alt="" style={imageStyle} />
+        </div>
       </div>
     </div>
   );
